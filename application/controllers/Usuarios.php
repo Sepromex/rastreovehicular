38f589d393384a0ef7746bb341bcab16cb569b99
Michelle Mendoza <michellemendozac94@gmail.com>
2021-01-27 16:58:37 -0600

    Eliminar index de la url

---
+++ application/controllers/Usuarios.php
@@ -0,0 +1,15 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Usuarios extends CI_Controller {
+
+	public function index()
+	{
+		echo "prueba de controlador";
+    }
+    
+    public function otra_prueba()
+	{
+		echo "prueba de controlador 2";
+	}
+}
